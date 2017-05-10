48d3f8f6bbf3236663c641958b54d253116d1c03
urota <bottlenome@gmail.com>
2017-05-11 00:13:14 +0900

    add updater

--- lstm_code_generation/seq2seq.py
+++ lstm_code_generation/seq2seq.py
@@ -1,7 +1,12 @@
 import numpy
+import argparse
 
-import chainer.links as L
 import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+
+EOS = 0
 
 
 class Encoder(chainer.Chain):
@@ -76,23 +81,63 @@ class Seq2Seq(chainer.Chain):
         self.decoder.h = h
         eos = numpy.array([-1 for _ in range(batch_size)], dtype=numpy.int32)
         t = chainer.Variable(eos)
-        for char in dec:
+        for i in range(dec.shape[1]):
+            char = dec[:, i]
             y.append(self.decoder(t))
             t = char
         return y
 
 
+class Seq2SeqUpdater(training.StandardUpdater):
+    def update_core(self):
+        train_iter = self.get_iterator('main')
+        optimizer = self.get_optimizer('main')
+
+        batch = train_iter.__next__()
+
+        enc, dec = self.converter(batch, self.device)
+
+        y = optimizer.target(enc, dec)
+
+        print(y)
+        print(dec)
+        assert(len(y) == dec.shape[1])
+        loss = 0
+        for i in range(len(y)):
+            loss += F.softmax_cross_entropy(y[i], dec[:, i])
+
+        optimizer.target.cleargrads()
+        loss.backward()
+        loss.unchain_backward()
+        optimizer.update()
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', '-b', type=int, default=20,
+                        help='Number of examples in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=39,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    args = parser.parse_args()
+
     model = Seq2Seq(10, 10)
-    x = numpy.array([[1, 2],
-                     [3, 3],
-                     [2, -1],
-                     [-1, 0]], dtype=numpy.int32)
-    x = chainer.Variable(x)
-    y = numpy.array([[1, 2],
-                     [3, 3],
-                     [2, -1],
-                     [-1, 0]], dtype=numpy.int32)
-    y = chainer.Variable(y)
-    for v in model(x, y):
-        print(v.data)
+    x = [[1, 2, -1], [3, 3, -1], [2, -1, 0]]
+    y = [[1, 2], [3, 3], [2, -1]]
+    train = []
+    for i in range(len(x)):
+        train.append((numpy.array(x[i], dtype=numpy.int32),
+                      numpy.array(y[i], dtype=numpy.int32)))
+
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    updater = Seq2SeqUpdater(train_iter, optimizer)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+    trainer.run()
